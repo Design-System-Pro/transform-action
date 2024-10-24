90ffabf9c9bcc22daf2aeacc4c5c6379ca604e33
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-10-24 16:48:02 +0200

    feat: reset versions and add pre release on next branch

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,5 @@
 const config = {
-  branches: ["main"],
+  branches: ["main", { name: "next", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
